2e0aa7ae2614c5b4d82cebc02120bab9d5040937
Katharine Chandler <katharinechandler@katharines-mbp.home>
2021-03-09 11:59:18 -0500

    working on fixing createItem

--- client/src/components/ItemCreate/ItemCreate.jsx
+++ client/src/components/ItemCreate/ItemCreate.jsx
@@ -1,36 +1,86 @@
 import { useState } from 'react';
+import { useHistory } from "react-router-dom";
+import { postItem } from '../../services/items'
 
 export default function ItemCreate(props) {
   const [formData, setFormData] = useState({
-    name: ''
-  });
-  const { name } = formData;
-  const { handleCreate } = props;
+    title: '',
+    // description: '',
+    // image_url: '',
+    // location: '',
+    // repository: '',
 
+  });
+  const [items, setItems] = useState({})
+  const { title} = formData;
+  const history = useHistory();
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData(prevState => ({
       ...prevState,
       [name]: value
     }))
-  }
+    const handleCreate = async (formData) => {
+      const newItem = await postItem(formData);
+      setItems(prevState => [...prevState, newItem]);
+      history.push('/items');
+    }  
+  
+    return (
 
-  return (
-    <form onSubmit={(e)=>{
-      e.preventDefault();
-      handleCreate(formData);
-    }}>
-      <h3>Create Item</h3>
-      <label>Name:
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        handleCreate(formData);
+      }}>
+        <h3>Create Item</h3>
+        <label>Title:
         <input
-          type='text'
-          name='name'
-          value={name}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-      <button>Submit</button>
-    </form>
-  )
+            type='text'
+            name='title'
+            value={title}
+            onChange={handleChange}
+          />
+        {/* </label>
+        <br />
+        <label>Description:
+        <input
+            type='text'
+            name='description'
+            value={description}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>Image URL
+        <input
+            type='text'
+            name='image_url'
+            value={image_url}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>Location
+        <input
+            type='text'
+            name='location'
+            value={location}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>Repository
+        <input
+            type='text'
+            name='repository'
+            value={repository}
+            onChange={handleChange}
+          /> */}
+        </label>
+        <br />
+        <button>Submit</button>
+        </form>
+        
+    )
+  }
 }
