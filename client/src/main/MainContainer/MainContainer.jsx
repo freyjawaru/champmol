c4baf1adbf9099c37922883d24b02d79d14c05e6
Katharine Chandler <katharinechandler@katharines-mbp.home>
2021-03-08 19:30:54 -0500

    got rid of errors in main component

--- client/src/main/MainContainer/MainContainer.jsx
+++ client/src/main/MainContainer/MainContainer.jsx
@@ -9,23 +9,23 @@ import ItemEdit from '../../components/ItemEdit/ItemEdit';
 import ItemDetail from '../../components/ItemDetail/ItemDetail';
 
 export default function MainContainer(props) {
-  const [Items, setItems] = useState([]);
-  const [Locations, setLocations] = useState([]);
+  const [items, setItems] = useState([]);
+  const [locations, setLocations] = useState([]);
   const { currentUser } = props;
   const history = useHistory();
 
   useEffect(() => {
     const fetchItems = async () => {
-      const ItemsList = await getAllItems();
-      setItems(ItemsList);
+      const itemsList = await getAllItems();
+      setItems(itemsList);
     }
     fetchItems();
   }, [])
 
   useEffect(() => {
     const fetchLocations = async () => {
-      const LocationsList = await getAllLocations();
-      setLocations(LocationsList);
+      const locationsList = await getAllLocations();
+      setLocations(locationsList);
     }
     fetchLocations();
   }, [])
@@ -33,7 +33,7 @@ export default function MainContainer(props) {
   const handleCreate = async (formData) => {
     const newItem = await postItem(formData);
     setItems(prevState => [...prevState, newItem]);
-    history.push('/Items');
+    history.push('/items');
   }
 
   const handleDelete = async (id) => {
@@ -43,42 +43,52 @@ export default function MainContainer(props) {
 
   const handleUpdate = async (id, formData) => {
     const updatedItem = await putItem(id, formData);
-    setItems(prevState => prevState.map((Item) => {
-      return Item.id === Number(id) ? updatedItem : Item
+    setItems(prevState => prevState.map((item) => {
+      return item.id === Number(id) ? updatedItem : item
     }));
-    history.push('/Items');
+    history.push('/items');
   }
 
   return (
+    <>
+      <h3>About</h3>
+      <p>The Chartreuse de Champmol was dedicated in 1383 by Margaret of Flanders, the Duchess of Burgundy. She and her husband, Philip the Bold, were active patrons of the monastery and both were personally involved in it throughout their lives. Philip was buried there: his coffin is surrounded with pleurants, or carved grieving monks made by Claus Sluter and his workshop. 
+      <br />
+      The monastery was destroyed in 1792. Some of the manuscripts and library made their way to Paris, where they were broken and sold by booksellers. Others remained in Dijon.
+      
+      <br />
+      The full design and art program of the monastery has been studied and written about by Sherry Lindquist in her well-known book, Agency, Visuality and Society at the Chartreuse de Champmol. The manuscripts, however, remain mostly unstudied. They are the focus of my future dissertation. The purpose of this site is to gather in one place the manuscripts made by and for the monks of the Chartreuse de Champmol.
+      </p>
     <Switch>
       {
         !currentUser &&
         <Redirect to='/' />
       }
-      <Route path='/Items/new'>
+      <Route path='/items/new'>
         <ItemCreate handleCreate={handleCreate} />
       </Route>
-      <Route path='/Items/:id/edit'>
+      <Route path='/items/:id/edit'>
         <ItemEdit
-          Items={Items}
+          items={items}
           handleUpdate={handleUpdate}
         />
       </Route>
-      <Route path='/Items/:id'>
-        <ItemDetail Locations={Locations} />
+      <Route path='/items/:id'>
+        <ItemDetail locations={locations} />
       </Route>
-      <Route path='/Items'>
+      <Route path='/items'>
         <Items
-          Items={Items}
+          items={items}
           currentUser={currentUser}
           handleDelete={handleDelete}
         />
       </Route>
-      <Route path='/Locations'>
+      <Route path='/locations'>
         <Locations
-          Locations={Locations}
+          locations={locations}
         />
       </Route>
-    </Switch>
+      </Switch>
+      </>
   )
 }
